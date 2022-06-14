1a60b08328bd082cea0ab51f7b468db45759500c
koxa17 <n.matusevich@clicktoid.ru>
2022-06-14 16:06:10 +0500

    delete

--- src/assets/api/myBase.js
+++
@@ -1,8 +0,0 @@
-import {push, ref, set} from "firebase/database";
-import {getCurrentDate} from "@/assets/js/date";
-
-
-export function writeArticlesData(url, data) {
-    data.created = getCurrentDate()
-    return set(url, data)
-}
